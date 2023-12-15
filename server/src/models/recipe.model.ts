34d6b9c06b97519d2856047263e41f3154d8f3b7
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-12-15 16:56:02 +0000

    added recipe model

---
+++ server/src/models/recipe.model.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { UpdateOptions } from "../utils/interfaces";
+
+const RecipeSchema = new mongoose.Schema({
+	title: { type: String, required: "Title is required" },
+	ingredients: [
+		{ type: String, required: "At least one ingredient is required" },
+	],
+	description: { type: String, required: "Description is required" },
+	image: { type: String, required: "Image is required" },
+	// image: { data: Buffer, contentType: String },
+	cookingTime: { type: Number, required: "Cooking time is required" },
+	updated: Date,
+	owner: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+	},
+	likes: { type: Number, default: 0 },
+	createddAt: { type: Date, default: Date.now },
+	// comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "comments" }],
+});
+
+export const RecipeModel = mongoose.model("Recipe", RecipeSchema);
+
+export const getAllRecipes = () =>
+	RecipeModel.find().populate("owner", "username").exec();
+
+export const getRecipeById = (id: string) =>
+	RecipeModel.findById(id).populate("owner", "username").exec();
+
+export const getRecipeByUser = (id: string) =>
+	RecipeModel.find({ owner: id }).populate("owner", "username").exec();
+
+export const deleteRecipeById = (id: string) =>
+	RecipeModel.findByIdAndDelete(id);
+
+export const updateRecipeById = (
+	id: string,
+	values: Record<string, any>,
+	options?: UpdateOptions
+) => RecipeModel.findByIdAndUpdate(id, values, options);
+
+export const createRecipe = (values: Record<string, any>) =>
+	new RecipeModel(values).save().then((recipe) => recipe.toObject());
