e20d86ed71e0d1f5e2b18dd2a635c83b0db9b3da
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-12-20 09:58:58 +0000

    minor changes

--- server/src/utils/inlineHandlers.ts
+++ server/src/utils/inlineHandlers.ts
@@ -1,42 +1,41 @@
 export interface Try<T> {
-  data: T | null
-  error: Error | null
+	data: T | null;
+	error: Error | null;
 }
 
 export interface StandardError {
-  error: string
-  message: string
+	error: string;
+	message: string;
 }
 
-export async function tryPromise<T>(func: Promise<T>): Promise<Try<T>>
-{
-  try {
-    const data = await func
-    
-    return <Try<T>>{data, error: null}
-  } catch (error) {
-    const result = <Try<T>>{data: null, error}
-    console.error(result.error?.message)
+// provides a standard way of dealing with success and error catching
+export async function tryPromise<T>(func: Promise<T>): Promise<Try<T>> {
+	try {
+		const data = await func;
 
-    return result
-  }
+		return <Try<T>>{ data, error: null };
+	} catch (error) {
+		const result = <Try<T>>{ data: null, error };
+		// console.error(result.error?.message);
+
+		return result;
+	}
 }
 
-export function trySync<T>(func: Function): Try<T>
-{
-  try {
-    return <Try<any>>{data: func(), error: null}
-  } catch (error) {
-    const result = <Try<any>>{data: null, error}
-    console.error(result.error?.message)
+export function trySync<T>(func: Function): Try<T> {
+	try {
+		return <Try<any>>{ data: func(), error: null };
+	} catch (error) {
+		const result = <Try<any>>{ data: null, error };
+		// console.error(result.error?.message);
 
-    return result
-  }
+		return result;
+	}
 }
 
 export function formatError(err: Error): StandardError {
-  return {
-    error: err.name,
-    message: err.message,
-  }
+	return {
+		error: err.name,
+		message: err.message,
+	};
 }
