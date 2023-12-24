fd89ad1bcd08db66916b6186c3f4322c422ce06d
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-12-24 08:12:02 +0000

    started recipes tests

---
+++ server/__tests__/recipes.test.ts
@@ -0,0 +1,42 @@
+import request from "supertest";
+import app from "../src/utils/app";
+import { HTTP_STATUS as statusCode } from "../src/utils/httpStatus";
+import { clearDB, closeDB, connectDB } from "./testdb";
+
+describe.skip("Recipes API Tests", () => {
+	beforeAll(async () => {
+		await connectDB();
+	});
+	afterAll(async () => {
+		await clearDB();
+		await closeDB();
+	});
+
+	// testing the post route, to create new recipes
+	describe("POST /:userId", () => {
+		it("should create new recipes", () => {
+			// generate and create a new user
+			// test creating a new recipe
+		});
+
+		it("should not create new recipes with missing fields", () => {
+			// generate and create a new user
+			// test creating a new recipe with missing fields
+		});
+	});
+
+	// get all recipes route
+	describe("GET /", () => {});
+
+	// get a specific user's recipes
+	describe("GET /get-user-recipes/:userId", () => {});
+
+	// get a single recipe
+	describe("GET /get-recipe/:id", () => {});
+
+	// update a specific recipe
+	describe("PUT /:userId/:recipeId", () => {});
+
+	// delete a specific recipe
+	describe("DELETE /:userId/:recipeId", () => {});
+});
