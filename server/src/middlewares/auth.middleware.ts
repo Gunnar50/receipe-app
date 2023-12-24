e6d18628982db592322945fd5a0f9c0dc7409c50
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-12-24 08:12:38 +0000

    updated to use try promise

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { deleteSession, getSessionById } from "../models/session.model";
 import { HTTP_STATUS as statusCode } from "../utils/httpStatus";
+import { formatError, tryPromise, trySync } from "../utils/inlineHandlers";
 import { AuthenticatedRequest } from "../utils/interfaces";
 
 // 15 minutes
@@ -12,65 +13,64 @@ export async function isAuthenticated(
 	res: express.Response,
 	next: express.NextFunction
 ) {
-	try {
-		// take the token from the cookies
-		const sessionToken = req.cookies["sessionToken"];
-		if (!sessionToken) {
-			return res.status(statusCode.UNAUTHORIZED).send({
-				message: "Unathorized.",
-			});
-		}
-
-		// get an user using the token
-		// const user = await getUserBySessionToken(sessionToken);
-
-		// try to find the corresponding token in the database
-		const currentSession = await getSessionById(sessionToken);
-		if (!currentSession) {
-			return res.status(statusCode.UNAUTHORIZED).send({
-				message: "Unathorized.",
-			});
-		}
-
-		// check if the session is expired
-		const now = new Date();
-		if (currentSession.expireAt < now) {
-			await deleteSession(sessionToken);
-			res.clearCookie("sessionToken");
-			return res.status(statusCode.UNAUTHORIZED).send({
-				message: "Session expired. Please login.",
-			});
-		}
-
-		const oneHourFromNow = new Date(now.getTime() + 60 * 60 * 1000);
-		if (currentSession.expireAt < oneHourFromNow) {
-			// if has less than 1 hour remaining, extend by 15 minutes
-			currentSession.expireAt = new Date(
-				currentSession.expireAt.getTime() + sessionAddedTime
-			);
-			await currentSession.save();
-		}
-
-		/**
-		 * What you could do instead of getting the user from the session token is decoding the token
-		 * and getting the user by ID. IRL, this is waaaay faster, safer, and better on the DB
-		 * You're using Mongo, so the get by the ID will be faster than the sessionToken.
-		 */
-		// if (!user) {
-		// 	return res.status(statusCode.UNAUTHORIZED).send({
-		// 		message: "Unathorized.",
-		// 	});
-		// }
-
-		// add userId to the request for next function
-		req.userId = currentSession.userId.toString();
-		return next();
-	} catch (error) {
-		console.log(error);
-		return res.status(statusCode.INTERNAL_SERVER_ERROR).send({
-			message: "Error: Something went wrong in our end.",
+	// take the token from the cookies
+	const sessionToken = req.cookies["sessionToken"];
+	if (!sessionToken) {
+		return res.status(statusCode.UNAUTHORIZED).send({
+			message: "Unathorized.",
 		});
 	}
+
+	// try to find the corresponding token in the database
+	const currentSession = await tryPromise(getSessionById(sessionToken));
+	if (currentSession.error) {
+		return res.status(statusCode.INTERNAL_SERVER_ERROR).send({
+			message: "Something went wrong",
+			errors: formatError(currentSession.error),
+		});
+	}
+
+	if (!currentSession.data) {
+		return res.status(statusCode.UNAUTHORIZED).send({
+			message: "Unathorized.",
+		});
+	}
+
+	// check if the session is expired
+	const now = new Date();
+	if (currentSession.data.expireAt < now) {
+		const { error } = await tryPromise(deleteSession(sessionToken));
+		if (error) {
+			return res.status(statusCode.INTERNAL_SERVER_ERROR).send({
+				message: "Something went wrong",
+				errors: formatError(error),
+			});
+		}
+
+		res.clearCookie("sessionToken");
+		return res.status(statusCode.UNAUTHORIZED).send({
+			message: "Session expired. Please login.",
+		});
+	}
+
+	const oneHourFromNow = new Date(now.getTime() + 60 * 60 * 1000);
+	if (currentSession.data.expireAt < oneHourFromNow) {
+		// if has less than 1 hour remaining, extend by 15 minutes
+		currentSession.data.expireAt = new Date(
+			currentSession.data.expireAt.getTime() + sessionAddedTime
+		);
+		const { error } = await tryPromise(currentSession.data.save());
+		if (error) {
+			return res.status(statusCode.INTERNAL_SERVER_ERROR).send({
+				message: "Something went wrong",
+				errors: formatError(error),
+			});
+		}
+	}
+
+	// add userId to the request for next function
+	req.userId = currentSession.data.userId.toString();
+	return next();
 }
 
 // this middleware is to allow the user to access their own data
@@ -79,23 +79,14 @@ export async function isOwner(
 	res: express.Response,
 	next: express.NextFunction
 ) {
-	try {
-		const { userId } = req.params;
-		// if you want to use this as a string, cast it to one
-		// const currentUserId = req.userId as string;
-		const currentUserId = req.userId as string;
+	const { userId } = req.params;
+	const currentUserId = req.userId as string;
 
-		if (currentUserId !== userId) {
-			return res.status(statusCode.UNAUTHORIZED).send({
-				message: "Unauthorized request.",
-			});
-		}
-
-		return next();
-	} catch (error) {
-		console.log(error);
-		return res.status(statusCode.INTERNAL_SERVER_ERROR).send({
-			message: "Error: Something went wrong in our end.",
+	if (currentUserId !== userId) {
+		return res.status(statusCode.UNAUTHORIZED).send({
+			message: "Unauthorized request.",
 		});
 	}
+
+	return next();
 }
