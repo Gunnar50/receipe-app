133cc5041f087718db256a87e6cca4db8b58cc3d
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-12-28 21:48:59 +0000

    remove traits

--- server/__tests__/factories/users.factory.ts
+++ server/__tests__/factories/users.factory.ts
@@ -1,24 +1,8 @@
 import { createUser } from "../../src/models/user.model";
 import { generateHash } from "../../src/utils/auth";
 
-interface factoryOptions {
-	traits: Array<string>;
-	attributes: Record<string, any>;
-}
-
-export async function createTestUser(
-	opts: factoryOptions = { traits: [], attributes: {} }
-) {
-	const attrsToAttach: Record<string, any> = userDefinition();
-
-	/**
-	 * If you decide you don't want traits, you can remove this
-	 */
-	if (opts.traits.length > 0) {
-		opts.traits.forEach((trait) => attachTrait(trait, attrsToAttach));
-	}
-
-	const userAttrs = Object.assign(attrsToAttach, opts.attributes);
+export async function createTestUser() {
+	const userAttrs: Record<string, any> = userDefinition();
 
 	return await createUser(userAttrs);
 }
@@ -29,25 +13,9 @@ export async function createTestUser(
  * number of characters.
  */
 function userDefinition(): Record<string, any> {
-	const data: Record<string, any> = {
+	return {
 		email: "test@example.com",
 		password: generateHash("password123"),
 		username: "testuser",
 	};
-
-	return data;
-}
-
-/**
- * Traits are for if you want to create templates of data you want on users.
- * There are much nicer ways of doing this, but if you're just experimenting,
- * this will work for a while.
- */
-function attachTrait(trait: string, attributes: Record<string, any>) {
-	switch (trait) {
-		case "admin":
-			attributes["email"] = "admin_user@admin_user.com";
-			break;
-		default:
-	}
 }
