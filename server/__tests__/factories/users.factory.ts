e4bd6c9c137998b66385e954280b95f56a7c0f24
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-01-03 16:47:53 +0000

    updating tests

--- server/__tests__/factories/users.factory.ts
+++ server/__tests__/factories/users.factory.ts
@@ -2,20 +2,9 @@ import { createUser } from "../../src/models/user.model";
 import { generateHash } from "../../src/utils/auth";
 
 export async function createTestUser() {
-	const userAttrs: Record<string, any> = userDefinition();
-
-	return await createUser(userAttrs);
-}
-
-/**
- * This is the basic user definition that will always be used when creating
- * a user. If you want random data, you could add faker, or use Uuid and access X
- * number of characters.
- */
-function userDefinition(): Record<string, any> {
-	return {
+	return await createUser({
 		email: "test@example.com",
 		password: generateHash("password123"),
 		username: "testuser",
-	};
+	});
 }
