1b9ccef5e5553260343883f6e1b10b6915ee05ef
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-01-04 11:16:38 +0000

    updated recipes factory

--- server/__tests__/factories/recipe.factory.ts
+++ server/__tests__/factories/recipe.factory.ts
@@ -1,17 +1,33 @@
-import { createNewRecipe } from "../../src/models/recipe.model";
+import { createNewRecipe, RecipeType } from "../../src/models/recipe.model";
+import { loginUserGetToken } from "../helper/loginUser";
+import { getRandomRecipe, TRecipeData } from "../helper/recipeData";
 
 export async function createRecipe(ownerId: string) {
-	const recipeAttr: Record<string, any> = recipeDefinition(ownerId);
-	return await createNewRecipe(recipeAttr);
+	const recipePayload: TRecipeData = getRandomRecipe();
+	recipePayload["owner"] = ownerId;
+
+	return await createNewRecipe(recipePayload);
 }
 
-function recipeDefinition(ownerId: string): Record<string, any> {
-	return {
-		title: "New Recipe Title",
-		ingredients: ["i1", "i2", "i3"],
-		description: "Test Taste",
-		image: "images/test.png",
-		cookingTime: 30,
-		owner: ownerId,
-	};
+/** factory to:
+ * create a new user
+ * login the user
+ * create a FEW recipes with this user
+ */
+
+export async function createRandomNumberOfRecipes() {
+	// create a user
+	const { user, sessionToken } = await loginUserGetToken();
+	// generate a random number between 1 and 10
+	const numberOfRecipes = Math.floor(Math.random() * 10) + 1;
+
+	// create an array of promises by calling createRecipe multiple times
+	const recipePromises: Promise<RecipeType>[] = [];
+	for (let i = 0; i < numberOfRecipes; i++) {
+		recipePromises.push(createRecipe(user._id.toString()));
+	}
+
+	const recipes: RecipeType[] = await Promise.all(recipePromises);
+
+	return { recipes, user, sessionToken };
 }
