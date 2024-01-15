aabf07a820452d5ad3b92c1f4701fa9648246b80
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-01-15 16:14:24 +0000

    recipe tests

--- server/src/utils/zodSchemas.ts
+++ server/src/utils/zodSchemas.ts
@@ -12,3 +12,21 @@ export const signupSchema = loginSchema.extend({
 		.string()
 		.min(3, { message: "Username must be at least 3 characters" }),
 });
+
+export const recipeSchema = z.object({
+	title: z
+		.string({ required_error: "Title is required." })
+		.min(3, { message: "Title needs to be at least 3 characters." }),
+	ingredients: z
+		.string()
+		.array()
+		.nonempty({ message: "At least one ingredient is required." }),
+	description: z
+		.string({ required_error: "Description is required." })
+		.min(3, { message: "Description needs to be at least 3 characters." }),
+	image: z.string({ required_error: "Image is required." }),
+	cookingTime: z
+		.number({ required_error: "Cooking time is required." })
+		.int()
+		.positive({ message: "Cooking time needs to be greater than zero." }),
+});
