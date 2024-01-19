f0b9cf9df948b3f00ae0c0d27800a4248ef9e433
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-01-19 16:07:45 +0000

    refractored store redux

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,5 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authSlice";
+import toastReducer from "./toastSlice";
 
-export const store = configureStore({
-	reducer: {},
+const store = configureStore({
+	reducer: {
+		auth: authReducer,
+		toast: toastReducer,
+	},
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
