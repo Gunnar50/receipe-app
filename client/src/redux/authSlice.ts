ef262185bf8591c82a4e0cb0234ff5ebb2a5cc53
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-01-21 16:58:32 +0000

    removed storage

--- client/src/redux/authSlice.ts
+++ client/src/redux/authSlice.ts
@@ -1,14 +1,8 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import {
-	getLocalStore,
-	removeLocalStore,
-	setLocalStore,
-} from "../utils/storage";
 
 interface UserState {
 	userId: string;
 	username: string;
-	sessionToken: string;
 }
 
 export interface AuthState {
@@ -30,20 +24,17 @@ const authSlice = createSlice({
 		login: (state, action: PayloadAction<UserState>) => {
 			state.isAuth = true;
 			state.user = action.payload;
-			setLocalStore<AuthState>("auth", state);
 		},
 
 		logout: (state) => {
 			state.isAuth = false;
 			state.user = null;
-			removeLocalStore("auth");
 		},
 
 		validateSession: (state, action: PayloadAction<boolean>) => {
 			if (!action.payload) {
 				state.isAuth = false;
 				state.user = null;
-				removeLocalStore("auth");
 			}
 		},
 	},
