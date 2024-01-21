d64cbfb2a95ec688786462a6b6cc65e35d9cbab7
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-01-21 16:58:54 +0000

    added credentials

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const baseURL: string = "http://localhost:6001";
-const API = axios.create({ baseURL });
+const API = axios.create({ baseURL, withCredentials: true });
 
 export default API;
