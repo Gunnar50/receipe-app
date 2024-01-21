099eae0cf40e17111daec68f0cb2ab63dcba43f4
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-01-21 16:58:38 +0000

    added persist

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,14 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+	FLUSH,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+	REHYDRATE,
+	persistReducer,
+	persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import authReducer from "./authSlice";
 import toastReducer from "./toastSlice";
 
-const store = configureStore({
-	reducer: {
-		auth: authReducer,
-		toast: toastReducer,
-	},
+const persistConfig = {
+	key: "root",
+	storage,
+};
+
+const rootReducer = combineReducers({
+	auth: persistReducer(persistConfig, authReducer),
+	toast: toastReducer,
+});
+
+export const store = configureStore({
+	reducer: rootReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 
-export default store;
+export const persistor = persistStore(store);
