37d00f708dbb1fc48e4ede728d42de9ba0e28f7d
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-01-21 16:57:36 +0000

    added persist gate

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,17 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.tsx";
 import "./index.css";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
 		<Provider store={store}>
-			<App />
+			<PersistGate persistor={persistor}>
+				<App />
+			</PersistGate>
 		</Provider>
 	</BrowserRouter>
 );
