fa1fe248dcebb7157228e0a1cf51a29c8aa61599
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-01-23 11:36:09 +0000

    form

--- client/src/components/authentication/AuthForm.tsx
+++ client/src/components/authentication/AuthForm.tsx
@@ -38,8 +38,8 @@ function AuthForm({
 
 			{/* SUBMIT BUTTON */}
 			<button
-				className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 transition-all"
-				type="submit"
+			// className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 transition-all"
+			// type="submit"
 			>
 				{label}
 			</button>
