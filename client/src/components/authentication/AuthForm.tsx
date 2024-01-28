f1d173168f3d5902b12ca129b80d3cde8576a35d
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-01-28 11:43:56 +0000

    new modals

--- client/src/components/authentication/AuthForm.tsx
+++ client/src/components/authentication/AuthForm.tsx
@@ -1,33 +1,33 @@
 import {
 	Anchor,
 	Button,
-	Container,
 	Divider,
 	Group,
 	Paper,
-	PaperProps,
 	PasswordInput,
 	Stack,
-	Text,
 	TextInput,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { upperFirst, useToggle } from "@mantine/hooks";
+import { upperFirst } from "@mantine/hooks";
 import axios from "axios";
 import { zodResolver } from "mantine-form-zod-resolver";
-import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { login } from "../../redux/authSlice";
 import { setContent } from "../../redux/toastSlice";
 import API from "../../utils/api";
 import { loginSchema, signupSchema } from "../../utils/zod";
 
-function AuthenticationForm(props: PaperProps) {
-	const { type: initialType } = useParams();
+interface AuthProps {
+	type: "login" | "register";
+	toggleType: () => void;
+	closeModal: () => void;
+}
+
+function AuthenticationForm({ closeModal, type, toggleType }: AuthProps) {
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
-	const [type, toggle] = useToggle(["login", "register"]);
 
 	const form = useForm({
 		initialValues: {
@@ -38,10 +38,6 @@ function AuthenticationForm(props: PaperProps) {
 
 		validate: zodResolver(type === "register" ? signupSchema : loginSchema),
 	});
-
-	useEffect(() => {
-		toggle(initialType);
-	}, [initialType]);
 
 	function handleError(error: unknown) {
 		if (axios.isAxiosError(error)) {
@@ -54,8 +50,6 @@ function AuthenticationForm(props: PaperProps) {
 			dispatch(setContent({ text: "Operation failed", type: "error" }));
 		}
 	}
-
-	// Use this handleError in your handleLogin and handleRegister
 
 	async function handleLogin(event: React.FormEvent<HTMLFormElement>) {
 		event.preventDefault();
@@ -76,6 +70,7 @@ function AuthenticationForm(props: PaperProps) {
 				})
 			);
 			navigate("/");
+			closeModal();
 		} catch (error: unknown) {
 			handleError(error);
 		}
@@ -110,77 +105,71 @@ function AuthenticationForm(props: PaperProps) {
 	}
 
 	return (
-		<Container size={450} my={40}>
-			<Paper radius="md" shadow="md" p="xl" withBorder {...props}>
-				<Text size="lg" fw={500}>
-					Welcome to RecipesApp, please {type}!
-				</Text>
+		<Paper>
+			<Divider labelPosition="center" mb="lg" />
 
-				<Divider labelPosition="center" my="lg" />
-
-				<form
-					onSubmit={(e) =>
-						type === "register" ? handleRegister(e) : handleLogin(e)
-					}
-				>
-					<Stack>
-						{type === "register" && (
-							<TextInput
-								required
-								placeholder="username"
-								value={form.values.username}
-								onChange={(event) =>
-									form.setFieldValue("username", event.currentTarget.value)
-								}
-								radius="md"
-							/>
-						)}
-
+			<form
+				onSubmit={(e) =>
+					type === "register" ? handleRegister(e) : handleLogin(e)
+				}
+			>
+				<Stack>
+					{type === "register" && (
 						<TextInput
 							required
-							placeholder="Email"
-							value={form.values.email}
+							placeholder="username"
+							value={form.values.username}
 							onChange={(event) =>
-								form.setFieldValue("email", event.currentTarget.value)
-							}
-							error={form.errors.email && "Invalid email"}
-							radius="md"
-						/>
-
-						<PasswordInput
-							required
-							placeholder="Password"
-							value={form.values.password}
-							onChange={(event) =>
-								form.setFieldValue("password", event.currentTarget.value)
-							}
-							error={
-								form.errors.password &&
-								"Password should include at least 6 characters"
+								form.setFieldValue("username", event.currentTarget.value)
 							}
 							radius="md"
 						/>
-					</Stack>
+					)}
 
-					<Group justify="space-between" mt="xl">
-						<Anchor
-							component="button"
-							type="button"
-							c="dimmed"
-							onClick={() => toggle()}
-							size="xs"
-						>
-							{type === "register"
-								? "Already have an account? Login"
-								: "Don't have an account? Register"}
-						</Anchor>
-						<Button type="submit" radius="xl">
-							{upperFirst(type)}
-						</Button>
-					</Group>
-				</form>
-			</Paper>
-		</Container>
+					<TextInput
+						required
+						placeholder="Email"
+						value={form.values.email}
+						onChange={(event) =>
+							form.setFieldValue("email", event.currentTarget.value)
+						}
+						error={form.errors.email && "Invalid email"}
+						radius="md"
+					/>
+
+					<PasswordInput
+						required
+						placeholder="Password"
+						value={form.values.password}
+						onChange={(event) =>
+							form.setFieldValue("password", event.currentTarget.value)
+						}
+						error={
+							form.errors.password &&
+							"Password should include at least 6 characters"
+						}
+						radius="md"
+					/>
+				</Stack>
+
+				<Group justify="space-between" mt="xl">
+					<Anchor
+						component="button"
+						type="button"
+						c="dimmed"
+						onClick={() => toggleType()}
+						size="xs"
+					>
+						{type === "register"
+							? "Already have an account? Login"
+							: "Don't have an account? Register"}
+					</Anchor>
+					<Button type="submit" radius="xl">
+						{upperFirst(type)}
+					</Button>
+				</Group>
+			</form>
+		</Paper>
 	);
 }
 
