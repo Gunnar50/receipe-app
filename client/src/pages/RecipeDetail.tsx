5e6bc914c96725e54a7b302c8e1cc49e91a4e9dd
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-02-03 17:49:56 +0000

    added description

--- client/src/pages/RecipeDetail.tsx
+++ client/src/pages/RecipeDetail.tsx
@@ -10,7 +10,9 @@ import {
 	Stack,
 	Text,
 	Title,
+	rem,
 } from "@mantine/core";
+import { IconBookmark, IconHeart } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import API from "../utils/api";
@@ -35,29 +37,64 @@ function RecipeDetail() {
 
 	if (!recipe) return "Loading...";
 	return (
-		<Grid>
+		<Grid gutter={"xl"}>
 			<Grid.Col span={{ xs: 5 }}>
 				<Image
 					src={recipe.image}
-					alt="Honey Roasted Peanut Butter"
-					style={{ width: "100%", height: "auto" }}
+					alt={recipe.title}
+					style={{ width: "100%", height: "auto", borderRadius: "7px" }}
 				/>
 			</Grid.Col>
 			<Grid.Col span={{ xs: 7 }}>
-				<Stack gap="xl">
-					<Group wrap="nowrap" align="flex-start" justify="space-between">
-						<Title order={2}>Honey Roasted Peanut Butter</Title>
-						<Button variant="light" color="blue" radius="md">
-							Save
-						</Button>
-					</Group>
+				<Stack justify="space-between" style={{ height: "100%" }}>
+					<Stack gap="0.5rem">
+						<Group wrap="nowrap" align="flex-start" justify="space-between">
+							<Title order={2}>{recipe.title}</Title>
+							<Group wrap="nowrap">
+								<Button
+									style={{ minWidth: "5rem" }}
+									variant="light"
+									color="red"
+									radius="md"
+									size="xs"
+								>
+									<IconHeart
+										style={{
+											width: rem(20),
+											height: rem(20),
+											marginRight: rem(5),
+										}}
+										stroke={1.5}
+									/>{" "}
+									Like
+								</Button>
+								<Button
+									style={{ minWidth: "5rem" }}
+									variant="light"
+									color="blue"
+									size="xs"
+									radius="md"
+								>
+									<IconBookmark
+										style={{
+											width: rem(20),
+											height: rem(20),
+											marginRight: rem(5),
+										}}
+										stroke={1.5}
+									/>{" "}
+									Save
+								</Button>
+							</Group>
+						</Group>
 
-					<Text size="sm" style={{ marginBottom: "md" }}>
-						by{" "}
-						<span style={{ fontWeight: 500 }}>
-							{recipe.owner?.username ? recipe.owner.username : "Unknown"}
-						</span>
-					</Text>
+						<Text size="sm" style={{ marginBottom: "md" }}>
+							by{" "}
+							<span style={{ fontWeight: 500 }}>
+								{recipe.owner?.username ? recipe.owner.username : "Unknown"}
+							</span>
+						</Text>
+					</Stack>
 
 					<Grid justify="space-around">
 						<Grid.Col
@@ -81,65 +118,24 @@ function RecipeDetail() {
 					</Grid>
 				</Stack>
 			</Grid.Col>
+			<Grid.Col span={{ xs: 5 }}>
+				<Box>
+					<Title order={3}>Ingredients</Title>
+					<List withPadding>
+						{recipe.ingredients.map((item, idx) => (
+							<List.Item key={idx}>{item}</List.Item>
+						))}
+					</List>
+				</Box>
+			</Grid.Col>
+			<Grid.Col span={{ xs: 7 }}>
+				<Box>
+					<Title order={3}>Description</Title>
+					<Text>{recipe.description}</Text>
+				</Box>
+			</Grid.Col>
 		</Grid>
 	);
-
-	// 	return (
-	// 		<Grid justify="center" style={{ marginTop: "20px" }}>
-	// 			<Grid.Col span={{ xs: 12, md: 10 }}>
-	// 				<Group justify="apart" style={{ marginBottom: "20px" }}>
-	// 					<Image
-	// 						src={recipe.image}
-	// 						alt="Recipe"
-	// 						style={{ maxWidth: "350px", borderRadius: "5px" }}
-	// 					/>
-
-	// 					<Box
-	// 						style={{
-	// 							flex: 1,
-	// 							marginLeft: "20px",
-	// 							marginRight: "20px",
-	// 							minWidth: "300px",
-	// 						}}
-	// 					>
-	// 						<Title order={2}>{recipe.title}</Title>
-	// 						<Text>
-	// 							by{" "}
-	// 							<strong>
-	// 								{recipe.owner?.username ? recipe.owner.username : "Unknown"}
-	// 							</strong>
-	// 						</Text>
-	// 						<Grid>
-	// 							<Grid.Col span={4}></Grid.Col>
-	// 							<Grid.Col span={4}></Grid.Col>
-	// 							<Grid.Col span={4}></Grid.Col>
-	// 						</Grid>
-	// 						{/* <Text>
-	// 							<strong>Cooking Time: </strong>
-	// 							{recipe.cookingTime} minutes
-	// 						</Text> */}
-	// 					</Box>
-	// 				</Group>
-
-	// 				<Group justify="apart" style={{ marginTop: "40px" }}>
-	// 					<Box style={{ flex: 1, marginLeft: "20px", marginRight: "20px" }}>
-	// 						<Title order={3}>Ingredients</Title>
-	// 						<ul>
-	// 							{recipe.ingredients.map((item, idx) => (
-	// 								<li key={idx}>{item}</li>
-	// 							))}
-	// 						</ul>
-	// 					</Box>
-
-	// 					<Box style={{ flex: 2, marginLeft: "20px", marginRight: "20px" }}>
-	// 						<Title order={3}>Description</Title>
-	// 						<Text>{recipe.description}</Text>
-	// 					</Box>
-	// 				</Group>
-	// 			</Grid.Col>
-	// 		</Grid>
-	// 	);
-	// }
 }
 
 export default RecipeDetail;
