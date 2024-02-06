70681a2b449b813bc06c9fa5ab85cf603d260879
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-02-06 16:40:53 +0000

    refractor auth modals

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import { Container } from "@mantine/core";
+import { useDisclosure, useToggle } from "@mantine/hooks";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import RecipeCard from "./components/Card/RecipeCard";
+import CustomModal from "./components/Nav/CustomModal";
 import Nav from "./components/Nav/Nav";
+import AuthenticationForm, {
+	AuthType,
+} from "./components/authentication/AuthForm";
 import Home from "./pages/Home";
 import NotFoundPage from "./pages/NotFound/NotFound";
 import RecipeDetail from "./pages/RecipeDetail";
@@ -21,7 +26,15 @@ function App() {
 	const dispatch = useDispatch();
 	const toastContent = useSelector(selectContent);
 	const user = useSelector(selectUser);
+	const [type, toggle] = useToggle<AuthType>(["login", "register"]);
 	const isAuthenticated = useSelector(selectIsAuthenticated);
+	const [openedAuth, { open: openAuth, close: closeAuth }] =
+		useDisclosure(false);
+
+	function triggerModal(type: "login" | "register") {
+		toggle(type);
+		openAuth();
+	}
 
 	useEffect(() => {
 		if (toastContent.text) {
@@ -51,13 +64,26 @@ function App() {
 	}, [user, isAuthenticated]);
 	return (
 		<>
+			<CustomModal
+				title={`Welcome to RecipesApp, please ${type}`}
+				opened={openedAuth}
+				close={closeAuth}
+			>
+				<AuthenticationForm
+					type={type}
+					toggleType={toggle}
+					closeModal={closeAuth}
+				/>
+			</CustomModal>
 			<ToastContainer />
-			<Nav />
+			<Nav triggerModal={triggerModal} />
 			<Container my="md">
 				<Routes>
 					<Route path="/" element={<Home />} />
-					<Route path="/recipe/:recipeId" element={<RecipeDetail />} />
-					{/* <Route path="/auth/:type" element={<AuthenticationForm />} /> */}
+					<Route
+						path="/recipe/:recipeId"
+						element={<RecipeDetail triggerModal={triggerModal} />}
+					/>
 					{/* <Route path="/create-recipe" /> */}
 					<Route path="*" element={<NotFoundPage />} />
 				</Routes>
