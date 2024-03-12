fce8828f31099ca4500ab2fa8ea671f9a898cc13
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-03-12 15:43:58 +0000

    started create recipe page

--- client/src/utils/zod.ts
+++ client/src/utils/zod.ts
@@ -12,3 +12,19 @@ export const signupSchema = loginSchema.extend({
 		.string()
 		.min(3, { message: "Username must be at least 3 characters" }),
 });
+
+export const createRecipeSchema = z.object({
+	title: z.string().min(3, { message: "Title must be at least 3 characters" }),
+	ingredients: z
+		.string()
+		.array()
+		.nonempty({ message: "Ingredients must contain at least 1 ingredient" }),
+	description: z
+		.string()
+		.min(3, { message: "Description must be at least 3 characters" }),
+	serves: z.number().gt(0, { message: "Serves must be at least 1 portion" }),
+	cookingTime: z
+		.number()
+		.gt(0, { message: "Cooking time must be greater than 1 minute." }),
+	image: z.string().min(1, { message: "Image must be at least 1 character." }),
+});
