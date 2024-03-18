704b6717c2a804910cf3bc3c1a1ec35a91bccaee
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-03-18 18:48:36 +0000

    updating create recipe page

--- server/src/utils/zodSchemas.ts
+++ server/src/utils/zodSchemas.ts
@@ -18,15 +18,16 @@ export const recipeSchema = z.object({
 		.string({ required_error: "Title is required." })
 		.min(3, { message: "Title needs to be at least 3 characters." }),
 	ingredients: z
-		.string()
-		.array()
-		.nonempty({ message: "At least one ingredient is required." }),
+		.array(z.string())
+		.nonempty({ message: "Ingredients must contain at least 1 ingredient" }),
 	description: z
 		.string({ required_error: "Description is required." })
 		.min(3, { message: "Description needs to be at least 3 characters." }),
 	image: z.string({ required_error: "Image is required." }),
+	serves: z.number().gt(0, { message: "Serves must be at least 1 portion" }),
 	cookingTime: z
 		.number({ required_error: "Cooking time is required." })
 		.int()
 		.positive({ message: "Cooking time needs to be greater than zero." }),
+	category: z.string().min(1, { message: "Must have a category." }),
 });
