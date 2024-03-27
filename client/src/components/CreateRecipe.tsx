96f7ef371e0cbe082ba4442aa4063fad65a5a00b
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-03-27 14:11:29 +0000

    update recipe forms

--- client/src/components/CreateRecipe.tsx
+++ client/src/components/CreateRecipe.tsx
@@ -1,6 +1,15 @@
+import { Paper, Title } from "@mantine/core";
 import RecipeForm from "./RecipeForm";
 
 function CreateRecipe() {
-	return <RecipeForm type="create" />;
+	return (
+		<Paper>
+			<Title mb={"2rem"} order={2}>
+				Create New Recipe
+			</Title>
+
+			<RecipeForm type="create" />
+		</Paper>
+	);
 }
 export default CreateRecipe;
