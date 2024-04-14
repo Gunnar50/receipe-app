a10d324718ad57f6737c2fcd66eb7194a61c9148
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-04-14 17:36:57 +0100

    update

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,11 @@
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import { persistor, store } from "@redux/store";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.tsx";
-import { persistor, store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
